75e54ce13ae3035699e64f9fc07fd05c9a770ffd
R3RO <paulocesarbritocamilo@gmail.com>
2023-05-26 11:02:53 -0300

    Menu fixed

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-const menu = window.document.getElementById('menu')
-const menu2 = window.document.getElementById('menu2')
+const menu = [...window.document.getElementsByClassName('menu')]
 const nav = window.document.getElementById('nav')
 
+/* Abrir e fechar menu   */
 
+for(let i = 0; i< menu.length; i++){
 
-menu.addEventListener ('click', function AbrirMenu() {
+   menu[i].addEventListener("click", function(){
 
-   menu.style.display = 'none'
-   menu2.style.display = 'inline-block'
-   nav.style.display = 'inline-block'
-})
+      if (menu[i] == menu[0]){
+         menu[0].style.display = "none"
+         menu[1].style.display = "inline-block"
+         nav.style.display = "block"
+      } else{
+         menu[1].style.display = "none"
+         menu[0].style.display = "inline-block"
+         nav.style.display = "none"
+      }
+      
 
-menu2.addEventListener ('click', function FecharMenu() {
-    menu2.style.display = 'none'
-    menu.style.display = 'inline-block'
-    nav.style.display = 'none'
- })
+   })
+
+}
